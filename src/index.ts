2842c5184a2c79f05fdb0bc6e6821bc01fed2906
Mike <84646003+mikemills254@users.noreply.github.com>
2026-03-10 12:26:31 +0300

    Add CORS methods and allowed headers

--- src/index.ts
+++ src/index.ts
@@ -41,6 +41,8 @@ app.use(cors({
         callback(new Error('Not allowed by CORS'));
     },
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
